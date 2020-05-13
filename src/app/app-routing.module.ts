3d141f7d6d67f7b09ead16963da41a6d61a18610
Jesus Finol <jfinol02@gmail.com>
2020-05-13 17:41:20 -0400

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
+import { RegisterComponent } from './login/register.component';
+import { PagesComponent } from './pages/pages.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { GraficasoneComponent } from './pages/graficasone/graficasone.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Routes, RouterModule } from '@angular/router';
 
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent},
+      { path: 'progress', component: ProgressComponent},
+      { path: 'graficauno', component: GraficasoneComponent},
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard'}
+    ]
+  },
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NopagefoundComponent}
+];
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash : true});
